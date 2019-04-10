5161be395c00b797b9db752cb0eaab8f1ada7be8
Ray-Arthur <27481892+StarZHF@users.noreply.github.com>
2019-04-10 15:27:17 -0500

    Add files via upload

---
+++ Combolist Maker/Combolist Maker/frmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Combolist_Maker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(13, 13);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.ReadOnly = true;
+            this.tbUser.Size = new System.Drawing.Size(291, 20);
+            this.tbUser.TabIndex = 0;
+            this.tbUser.Text = "Select Usernames...";
+            this.tbUser.DoubleClick += new System.EventHandler(this.tbUser_DoubleClick);
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(13, 40);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.ReadOnly = true;
+            this.tbPass.Size = new System.Drawing.Size(291, 20);
+            this.tbPass.TabIndex = 1;
+            this.tbPass.Text = "Select Passwords...";
+            this.tbPass.DoubleClick += new System.EventHandler(this.tbPass_DoubleClick);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(229, 66);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Combine";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 98);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Combolist maker | UID: 69588";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ Combolist Maker/Combolist Maker/frmMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combolist_Maker
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            MaximumSize = Size;
+            MinimumSize = Size;
+        }
+        
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread t = new Thread(() => combineLists(File.ReadAllLines(tbUser.Text), File.ReadAllLines(tbPass.Text)));
+                t.IsBackground = true;
+                t.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void combineLists(String[] list1, String[] list2)
+        {
+            List<String> toReturn = new List<String>();
+            foreach (String user in list1)
+            {
+                foreach (String pass in list2)
+                {
+                    toReturn.Add(String.Format("{0}:{1}", user, pass));
+                }
+            }
+            File.AppendAllLines(Environment.CurrentDirectory + @"\Combined.txt", toReturn);
+            MessageBox.Show("Success!");
+        }
+
+        private void tbUser_DoubleClick(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    tbUser.Text = ofd.FileName;
+                }
+            }
+
+        }
+
+        private void tbPass_DoubleClick(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    tbPass.Text = ofd.FileName;
+                }
+            }
+
+        }
+    }
+}
